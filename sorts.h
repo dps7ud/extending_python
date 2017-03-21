806cdeca9511e014a3d91d58e10b157726b61211
dps7ud <dps7ud@virginia.edu>
2017-03-21 18:02:42 -0400

    mt_wrapper is a wrapper for mt_sort

--- sorts.h
+++ sorts.h
@@ -9,3 +9,4 @@ void print_array(int array[], int size);
 void merge(int left[], int lmax, int right[], int rmax);
 void sort(int array[], int size);
 void* mt_sort(void* param);
+int mt_wrapper(int* array, int len);
